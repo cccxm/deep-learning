e77228f3615c2c55553df346290cea107728b6ec
cccxm <1085005134@qq.com>
2020-08-01 15:00:24 +0800

    clear project

--- pytorch/nn/pooling-layers/FractionalMaxPool2d.py
+++ pytorch/nn/pooling-layers/FractionalMaxPool2d.py
@@ -1,6 +1,5 @@
 import torch
 
-# TODO 详细查询实现原理
 m = torch.nn.FractionalMaxPool2d(kernel_size=2, output_size=1)
 input = torch.tensor([[[[1, 2, 4],
                         [4, 2, 1],
---
+++ pytorch/tensor/Creations.py
@@ -0,0 +1,7 @@
+import torch
+import numpy as np
+
+# 1 通过tensor函数创建
+
+tensor = torch.tensor([1, 2, 3, 4])
+print(tensor)
